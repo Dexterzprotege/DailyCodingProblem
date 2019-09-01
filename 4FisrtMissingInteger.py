0799e6dc9845a09d4682598bd748fee1694b6854
Dexterzprotege <42378552+Dexterzprotege@users.noreply.github.com>
2019-09-01 13:00:14 +0530

    Update 4FisrtMissingInteger.py

--- 4FisrtMissingInteger.py
+++ 4FisrtMissingInteger.py
@@ -1,3 +1,7 @@
+# This problem was asked by Stripe.
+# Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+# For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+# You can modify the input array in-place.
 def miss(arr,n):
     for i in range(n):
         if arr[i]<=0:
@@ -11,6 +15,7 @@ def miss(arr,n):
     for i in range(n):
         if arr[i]>0:
             return i+1
-    return n+1;
+    return n+1
+
 arr=[int(x) for x in input("Enter array: ").split()]
 print("First missing element in array is:",miss(arr,len(arr)))
