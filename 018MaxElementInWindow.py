e4f22b4e380b4c451a58653b8bae4ac42c5d4e92
Dexterzprotege <42378552+Dexterzprotege@users.noreply.github.com>
2019-09-08 19:18:46 +0530

    Add files via upload

---
+++ 018MaxElementInWindow.py
@@ -0,0 +1,37 @@
+# This problem was asked by Google.
+# Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each subarray of length k.
+# For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7, 8, 8], since:
+# •	10 = max(10, 5, 2)
+# •	7 = max(5, 2, 7)
+# •	8 = max(2, 7, 8)
+# •	8 = max(7, 8, 7)
+# Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results. You can simply print them out as you compute them.
+
+def maxinwind(arr,n,k):
+    hs=dict()
+    res=[]
+    for i in range(k):
+        if arr[i] not in hs:
+            hs[arr[i]]=1
+        else:
+            hs[arr[i]]+=1
+    res.append(max(hs))
+    i=0
+    j=k
+    while j<n:
+        c = arr[i]
+        hs[c] = hs[c] - 1
+        if (hs[c] == 0):
+            del hs[c]
+        if arr[j] not in hs:
+            hs[arr[j]] = 1
+        else:
+            hs[arr[j]] += 1
+        res.append(max(hs))
+        i+=1
+        j+=1
+    return res
+
+arr=[int(x) for x in input("Enter array elements: ").split()]
+k=int(input("Enter window size: "))
+print("Max ele are:",maxinwind(arr,len(arr),k))
